8548f12280bd9c7c6ecd35d2dddc97bbb9d3b188
PashaBalandin <pashabalandin1200@gmail.com>
2020-11-04 14:18:07 +0200

    Spring Boot API project has been created

---
+++ src/main/java/flightassistant/FlightAssistantApplication.java
@@ -0,0 +1,13 @@
+package flightassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flightassistant/FlightAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package flightassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
